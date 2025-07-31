bce160d230bbc96d992848608d493f1f6a62dc68
ParamveerSingh2 <paramveer.kody@gmail.com>
2025-07-31 13:57:49 +0530

    update app_colors
    app colors updated for app bar

--- lib/ui/utils/theme/app_colors.dart
+++ lib/ui/utils/theme/app_colors.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   AppColors._();
+
   static const Color black = Color(0xff000000);
+  static const Color clr576464 = Color(0xff576464);
+  static const Color clrB5B5B5 = Color(0xffB5B5B5);
+  static const Color clr131414 = Color(0xff131414);
   static const Color clrF0F5F9 = Color(0xffF0F5F9);
   static const Color clrF8F8F8 = Color(0xffF8F8F8);
-  
 }
